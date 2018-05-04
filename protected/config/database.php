cf6346ffaf9fd82aef7eb45e29588aee5cd10070
Abel Derviche - TTS Viajes <aderviche@ttsviajes.com>
2018-05-04 16:56:40 -0300

    first commit

---
+++ protected/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	'connectionString' => 'mysql:host=localhost;dbname=showfand_yii',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => '',
+	'charset' => 'utf8',
+);
